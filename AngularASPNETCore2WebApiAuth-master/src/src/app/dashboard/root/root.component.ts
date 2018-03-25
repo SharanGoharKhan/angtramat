f8ed225ae540724e24c44c3a41c410e9b69c4123
Waleed <wali@an10.io>
2018-03-26 00:13:56 +0500

    fixed minor issues

--- AngularASPNETCore2WebApiAuth-master/src/src/app/dashboard/root/root.component.ts
+++ AngularASPNETCore2WebApiAuth-master/src/src/app/dashboard/root/root.component.ts
@@ -5,6 +5,8 @@ import { PortalScreenService } from '../../../clientportal/api/portalScreen.serv
 import { PortalScreen } from '../../../clientportal/model/portalScreen';
 import { DashboardService } from '../services/dashboard.service';
 import {MatIconRegistry} from '@angular/material';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
@@ -16,11 +18,13 @@ import {DomSanitizer} from '@angular/platform-browser';
 export class RootComponent implements OnInit {
 
   private side_bar_items:SidebarItem[] = [];
+  active_item: any = null
   first_time_portal_screen = 0
   constructor(
     private sidebarItemService: SidebarItemService,
     private portalScreenService: PortalScreenService,
     private dashboardService: DashboardService,
+    private route: ActivatedRoute, private router: Router,
     public iconRegistry: MatIconRegistry, public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
@@ -30,6 +34,7 @@ export class RootComponent implements OnInit {
     .subscribe((data: PortalScreen[])=>{
       this.side_bar_items = data;
       console.log(this.side_bar_items)
+      this.active_item = this.side_bar_items[0]
       for (let index = 0; index < this.side_bar_items.length; index++) {
         // console.log(this.side_bar_items[index].iconUrl.search('.svg'))
         if (this.side_bar_items[index].iconUrl && this.side_bar_items[index].iconUrl.indexOf('.svg') > -1) {
@@ -44,10 +49,18 @@ export class RootComponent implements OnInit {
     })
   }
   sideBarItemClick(sidebarItem: PortalScreen) {
+    this.active_item = sidebarItem
     this.dashboardService.setPortalScreen(sidebarItem)
     this.first_time_portal_screen += 1
     if(this.first_time_portal_screen > 2)
       this.first_time_portal_screen = 0
+  }
+
+  checkActiveRoute(item: any) {
+    if(item.title === this.active_item.title) {
+      return true
+    }
+    return false
   }
 
 }
